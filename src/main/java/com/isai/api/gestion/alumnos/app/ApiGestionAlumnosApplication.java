40ba8d6828d2cc1d7f9b579db648a6d4fbe727c7
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-12-31 11:29:40 -0500

    clase main

---
+++ src/main/java/com/isai/api/gestion/alumnos/app/ApiGestionAlumnosApplication.java
@@ -0,0 +1,13 @@
+package com.isai.api.gestion.alumnos.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestionAlumnosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGestionAlumnosApplication.class, args);
+    }
+
+}
