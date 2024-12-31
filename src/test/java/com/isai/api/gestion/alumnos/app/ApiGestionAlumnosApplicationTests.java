8cfad8ba7416cc7beb95fd92d49ce881af445d94
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-12-31 11:28:34 -0500

    confuguracion del proyecto

---
+++ src/test/java/com/isai/api/gestion/alumnos/app/ApiGestionAlumnosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isai.api.gestion.alumnos.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestionAlumnosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
